3464cd464fcc21e6ad74a82287a58222b1f19789
quynhu_d <dqnhu00@gmail.com>
2021-12-01 16:37:23 +0300

    batch to device fix

--- data/data_utils.py
+++ data/data_utils.py
@@ -15,9 +15,11 @@ class Batch:
     duration_preds: Optional[torch.Tensor] = None
 
     def to(self, device: torch.device) -> 'Batch':
-        self.waveform.to(device)
-        self.waveform_length.to(device)
-        self.tokens.to(device)
-        self.token_lengths.to(device)
+        self.waveform = self.waveform.to(device)
+        self.waveform_length = self.waveform_length.to(device)
+        self.tokens = self.tokens.to(device)
+        self.token_lengths = self.token_lengths.to(device)
         if self.durations:
-            self.durations.to(device)
+            self.durations = self.durations.to(device)
+        if self.duration_preds:
+            self.duration_preds = self.duration_preds.to(device)
